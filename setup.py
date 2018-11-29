bedb0a8de71b09d39c0a057d5863b36fd6961590
Quan Lin <36973354+jacklinquan@users.noreply.github.com>
2018-11-29 16:51:33 +1100

    Version 0.1.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="usbserial4a",
-    version="0.1.1",
+    version="0.1.2",
     description="Python package for Kivy Android USB serial port.",
     long_description="https://github.com/jacklinquan/usbserial4a",
     long_description_content_type="text/markdown",
